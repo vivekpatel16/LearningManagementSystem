0f0a931d4a76ad232220bb84c32493a35d5eb077
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-02-21 17:49:37 +0530

    Delete Backend/routes/userInfoRoutes.js

--- Backend/routes/userInfoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { registerUser, loginUser, getAllUsers } = require("../controllers/userInfoController");
-const router = express.Router();
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/all", getAllUsers);
-
-module.exports = router;
