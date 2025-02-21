32acbe35547dc21b176ad7affb135bbd7ec011cc
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-02-21 17:48:19 +0530

    Add files via upload
    Created API for login ,register, get Users and added authentication using JWT token

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connectDb = require('./config/db');
-const userRoutes=require("./routes/userInfoRoutes");
+const userRoutes=require("./routes/commonRoutes");
+const adminRoutes=require("./routes/adminRoutes");
+
 connectDb();
 const app = express();
 app.use(express.json());
 
 app.use("/api/users",userRoutes);
+app.use("/api/admin",adminRoutes);
 
 const PORT =5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
