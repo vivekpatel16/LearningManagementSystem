85489a58c6de92df8e0bd29077093891b45a148d
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-03-03 21:06:08 +0530

    Update commonRoutes.js

--- Backend/routes/commonRoutes.js
+++ Backend/routes/commonRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post("/login", loginUser);
 router.get("/courses",authenticateUser, fetchCourses);
+router.patch("/profile",authenticateUser,updateProfile);
+router.patch("/delete-image",authenticateUser,deleteUserImage);
 module.exports = router;
