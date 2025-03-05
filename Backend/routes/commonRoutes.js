e77085cf440064a28c3872bc9afc5311e37b6848
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-03-05 13:19:52 +0530

    Update commonRoutes.js

--- Backend/routes/commonRoutes.js
+++ Backend/routes/commonRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { loginUser } = require("../controllers/commonController");
+const { loginUser,updateProfile,deleteUserImage } = require("../controllers/commonController");
 const { fetchCourses } = require("../controllers/coursesController");
 const { authenticateUser } = require("../middleware/authUserMiddleware")
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.get("/courses",authenticateUser, fetchCourses);
+router.patch("/profile",authenticateUser,updateProfile);
+router.patch("/delete-image",authenticateUser,deleteUserImage);
 module.exports = router;
