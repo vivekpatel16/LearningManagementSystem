b9cb5634b1dabb1fb8d984446a78654babb8d56e
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-03-05 13:22:13 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,8 +11,8 @@ connectDb();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/users",commonRoutes);
