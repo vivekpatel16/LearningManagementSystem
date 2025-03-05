b5a66ed5f218554ff24ccb74f1a444f818cfc518
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-03-05 13:26:34 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const connectDb = require('./config/db');
 const commonRoutes=require("./routes/commonRoutes");
 const adminRoutes=require("./routes/adminRoutes");
 const coursesRoutes = require("./routes/coursesRoutes");
-const path=require("path");
+
 dotenv.config();
 connectDb();
 
@@ -13,11 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/users",commonRoutes);
 app.use("/api/admin",adminRoutes);
 app.use("/api/courses",coursesRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const PORT =5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
