bc7740882226e3935bbed6168eb8078fd875d975
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-03-05 13:19:30 +0530

    Update commonController.js

--- Backend/controllers/commonController.js
+++ Backend/controllers/commonController.js
@@ -20,3 +20,48 @@ exports.loginUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.updateProfile = async(req,res)=>{
+  try {
+    const userId = req.user.id; // Extracted from token
+    const { user_name, password, user_image } = req.body;
+
+    // Find user by ID
+    let user = await UserInfo.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update only provided fields
+    if (user_name) user.user_name = user_name;
+    if (user_image) user.user_image = user_image;
+
+    if (password) {
+      // const salt = await bcrypt.genSalt(10);
+      // user.password = await bcrypt.hash(password, salt);
+      user.password = password;
+    }
+
+    await user.save();
+    res.json({ message: "Profile updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.deleteUserImage = async(req,res)=>{
+  try {
+    const { userId } = req.body;
+    const user = await UserInfo.findById(userId);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Remove image from the database
+    user.user_image = "";
+    await user.save();
+
+    res.json({ message: "Profile image deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
