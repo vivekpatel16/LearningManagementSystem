1ae70216f347c7f8efb6aa9ca499087b506f978e
Hensypatel <125814182+Hensypatel@users.noreply.github.com>
2025-03-07 16:28:56 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,8 +10,6 @@ dotenv.config();
 connectDb();
 
 const app = express();
-// app.use(cors());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ limit: "200mb", extended: true }));
