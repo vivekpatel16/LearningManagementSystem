db4e675aaa578347c600fd03757872ef1b9542a6
Hensypatel <125814182+Hensypatel@users.noreply.github.com>
2025-03-25 10:11:26 +0530

    Update wishlistController.js

--- Backend/controllers/wishlistController.js
+++ Backend/controllers/wishlistController.js
@@ -1,21 +1,24 @@
 const Wishlist=require("../models/wishlistModel");
 
-exports.addToWishlist=async(req,res)=>
+exports.toggleWishlist=async(req,res)=>
 {
     try{
        const {user_id,course_id}=req.body;
         const existingCourse=await Wishlist.findOne({user_id,course_id});
-        if(!existingCourse)
+        if(existingCourse)
         {
+            await Wishlist.deleteOne({ user_id, course_id });
             return res.status(400).json({message:"course already in wishlist"});
         }
+       else{
         const wishlistItem=new Wishlist({user_id,course_id});
         await wishlistItem.save();
         res.status(201).json({message:"course added to wishlist",wishlistItem});
+       }
     }
     catch(error)
     {
-        console.log("server error while adding to wishlist");
+        console.log("server error while adding to wishlist",error);
         res.status(500).json({message:"server error while adding to wishlist"});
     }
 }
@@ -31,30 +34,11 @@ exports.getWishlist=async(req,res)=>
             res.status(404).json({message:"user not found"});
        }
        const wishlist= await Wishlist.findOne({user_id}).populate("course_id");
-       res.status(200).json({message:"wishlist fetched"},wishlist);
+       res.status(200).json({message:"wishlist fetched",wishlist});
     }
     catch(error)
     {
-        console.log("server error while adding to wishlist");
+        console.log("server error while adding to wishlist",error);
         res.status(500).json({message:"server error while adding to wishlist"});
-    }
-};
-
-
-exports.deleteWishlist=async(req,res)=>
-{
-    try{
-        const {user_id,course_id}=req.params;
-        const deleteItem=await Wishlist.findOneAndDelete({user_id,course_id});
-        if(!deleteItem)
-        {
-            return res.status(404).json({message:"course not found in wishlist"});
-        }
-        res.status(200).json({message:"course removed from wishlist"});
-    }
-    catch(error)
-    {
-        console.log("server error while deleting from wishlist");
-        res.status(500).json({message:"server error while deleting from wishlist"});
     }
 };
