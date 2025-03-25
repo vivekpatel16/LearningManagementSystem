eed2daac25020594fc58bbb59e527bf0b0447e59
Hensypatel <125814182+Hensypatel@users.noreply.github.com>
2025-03-25 10:57:02 +0530

    Update wishlistRoutes.js

--- Backend/routes/wishlistRoutes.js
+++ Backend/routes/wishlistRoutes.js
@@ -1,11 +1,10 @@
 const express=require("express");
 const router=express.Router();
 const { authenticateUser } = require("../middleware/authUserMiddleware");
-const {addToWishlist,getWishlist,deleteWishlist}=require("../controllers/wishlistController");
+const {toggleWishlist,getWishlist, }=require("../controllers/wishlistController");
 
 
-router.post("/add",authenticateUser,addToWishlist);
-router.get("/",authenticateUser,getWishlist);
-router.delete("/:course_id",authenticateUser,deleteWishlist);
+router.post("/add",authenticateUser,toggleWishlist);
+router.get("/:user_id",authenticateUser,getWishlist);
 
 module.exports=router;
