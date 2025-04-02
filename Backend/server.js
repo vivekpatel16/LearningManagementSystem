298ac26918dea135af7b10c6b6cd55e8b607980c
Hitpatel02 <162440359+Hitpatel02@users.noreply.github.com>
2025-04-02 17:29:25 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ connectDb();
 
 const app = express();
 
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: 'https://learningmanagementsystem-6yfw.onrender.com/' , credentials: true }));
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use("/uploads",express.static("uploads"));
