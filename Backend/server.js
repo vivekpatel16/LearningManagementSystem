ccacccde11fd796fbbe815e5aa7f82439fe7d559
Hensypatel <125814182+Hensypatel@users.noreply.github.com>
2025-04-04 10:31:38 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors=require("cors");
-const path = require("path");
+
 const connectDb = require('./config/db');
 const commonRoutes=require("./routes/commonRoutes");
 const adminRoutes=require("./routes/adminRoutes");
