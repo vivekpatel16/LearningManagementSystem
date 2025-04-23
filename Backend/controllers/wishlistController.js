5499a2f8a1ed51999d05c0e5cd9231b433adf01f
vivekpatel16 <144759211+vivekpatel16@users.noreply.github.com>
2025-04-23 16:28:10 +0530

    Delete Backend directory

--- Backend/controllers/wishlistController.js
+++
@@ -1,9 +0,0 @@
-const Courses=require("../models/coursesInfoModel");
-const User=require("../models/userInfoModel");
-
-exports.addToWishlist=async(req,res)=>
-{
-    const {course_id}=req.body;
-    const user_id=req.user.id;
-    
-}
