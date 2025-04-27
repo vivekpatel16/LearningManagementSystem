73b3c84c30a06bb8bcf0cb62b60a0fd033ea2dc1
Hensypatel <125814182+Hensypatel@users.noreply.github.com>
2025-04-27 22:03:50 +0530

    Delete frontend directory

--- frontend/src/Api/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "https://learningmanagementsystem-2-bj3z.onrender.com/api", // Replace with your backend URL
-  withCredentials: true, // If you're using authentication
-});
-
-export default axiosInstance;
